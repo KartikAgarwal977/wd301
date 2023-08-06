ae7e4032268cee4ba4f8cad6a3e78e6486b83fd5
KartikAgarwal977 <101928227+KartikAgarwal977@users.noreply.github.com>
2023-08-06 14:41:09 +0530

    level-1

--- hello-react/src/TaskCard.jsx
+++ hello-react/src/TaskCard.jsx
@@ -4,7 +4,7 @@ const TaskCard = (props) => {
   return (
     <div className='TaskItem'>
         <h2 className='text-xl font-bold'>{props.title} </h2>
-      {date == null ? "Due on :"+props.dueDate : "Completed on :" + props.completedAtDate}
+      {date == null ? "due date:"+props.dueDate : "completed on:" + props.completedAtDate}
         <p>Assignee: {props.assigneeName} </p>
       </div>
     )
