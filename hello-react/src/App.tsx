e011079ebba694697d2f40ca1f75f58d81c2d7d2
KartikAgarwal977 <101928227+KartikAgarwal977@users.noreply.github.com>
2023-08-08 22:21:00 +0530

    level2

--- hello-react/src/App.tsx
+++ hello-react/src/App.tsx
@@ -1,8 +1,7 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import TaskCard from "./TaskCard";
 import './TaskCard.css'
+import React from "react";
 function App() {
   return (
     <>
--- hello-react/src/TaskCard.tsx
+++ hello-react/src/TaskCard.tsx
@@ -1,9 +1,11 @@
+import React from 'react'
 import './TaskCard.css'
 const TaskCard = (props) => {
+  let date: string = props.completedAtDate;
   return (
     <div className='TaskItem'>
         <h2 className='text-xl font-bold'>{props.title} </h2>
-      {date == null ? "due date:"+props.dueDate : "completed on:" + props.completedAtDate}
+      {date == null ? "Due on: "+props.dueDate : "Completed on: " + props.completedAtDate}
         <p>Assignee: {props.assigneeName} </p>
       </div>
     )
