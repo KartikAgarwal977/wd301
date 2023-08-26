b5212319abb8fcb4330bdd83d77d7481838d4011
KartikAgarwal977 <101928227+KartikAgarwal977@users.noreply.github.com>
2023-08-26 23:31:56 +0530

    level-4.2

--- smarter-tasks/src/Task.tsx
+++ smarter-tasks/src/Task.tsx
@@ -23,7 +23,7 @@ const Task = (props: Taskprop) => {
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 24 24"
             fill="currentColor"
-            className="w-6 h-6 deleteTaskButton"
+            className="w-6 h-6"
           >
             <path
               // fill-rule="evenodd"
