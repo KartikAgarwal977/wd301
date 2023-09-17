abed6444a95db3ef308d3d0973060d655662e29c
KartikAgarwal977 <101928227+KartikAgarwal977@users.noreply.github.com>
2023-09-18 00:10:54 +0530

    level-6 remove file

--- smarter-tasks/src/App.tsx
+++ smarter-tasks/src/App.tsx
@@ -1,21 +1,14 @@
 import "./App.css";
-import HomePage from "./pages/HomePage";
-import TaskListPage from "./pages/TaskListPage";
-import TaskDetailsPage from "./pages/TaskDetailsPage";
-// import TaskApp from "./TaskApp";
-import Form from "./Form";
-import ReactPlayground from "./ReactPlayground";
-import Signin from "./pages/Signin";
 import {
   createBrowserRouter,
   Navigate,
   RouterProvider,
 } from "react-router-dom";
-import Layout from "./Layout";
 import Dashboard from "./pages/dashboard";
 import ProtectedRoute from "./ProtectedRoute";
 import Notfound from "./pages/Notfound";
 import Signup from "./pages/signup";
+import Signin from "./pages/signin";
 
 // import TaskList from "./TaskList";
 const router = createBrowserRouter([
@@ -71,7 +64,6 @@ const router = createBrowserRouter([
 const App = () => {
   return (
     <div>
-      <Form />
       {/* <ReactPlayground /> */}
       <RouterProvider router={router} />
     </div>
